a3983a88850819e6b2463b0956d96f4904671d9e
Gareth Allan <sonikblue@users.noreply.github.com>
2017-09-02 05:34:57 +0100

    Initial Spring Boot app.

---
+++ src/main/java/uk/co/sonikblue/msgusers/MsgUsersApplication.java
@@ -0,0 +1,12 @@
+package uk.co.sonikblue.msgusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgUsersApplication.class, args);
+	}
+}
